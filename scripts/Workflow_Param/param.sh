f447a3d135180d24d74aa9a7ec93c242cfee69cd
CRCWORK3 <CRCWORK3@login0a.htc.sam.pitt.edu>
2017-09-27 09:37:08 -0400

    Changed privilages

--- scripts/Workflow_Param/param.sh
+++ scripts/Workflow_Param/param.sh
@@ -3,28 +3,25 @@ show_help() {
 cat << EOF
 Usage: ${0##*/} [-s <SRR ID>] [-d <Path to BLAST Database>] [-c <Custom Parameters>] [-h <help>]…
 Do stuff with FILE and write the result to standard output. With no FILE or when FILE is -, read standard input.
-        -h          display this help and exit
-        -c          Custom Parameters for Assembly, filtering, and BLASTing
-        -s          The ID of an SRR read number is needed to perform BUD
-        -d          Path to Viral RefSeq Database needed to pull viral contigs
+	-h          display this help and exit
+	-c 	    Custom Parameters for Assembly, filtering, and BLASTing
+	-s 	    The ID of an SRR read number is needed to perform BUD
+	-d          Path to Viral RefSeq Database needed to pull viral contigs
 
 EOF
 }
 
-while getopts “h:s:d:” o; do
+while getopts “:s:d:” o; do
     case "${o}" in
-        h)
-            show_help
-            exit 0 ;;
         s)
             s=${OPTARG}
             ;;
-	d)
+        d)
             d=${OPTARG}
             ;;
-	*)
-            show_help >&3
-            exit 1
+        *)
+            show_help >&2;
+	    exit 1
             ;;
     esac
 done
@@ -32,12 +29,13 @@ done
 shift "$((OPTIND-1))"
 
 echo “Pulling Viral Reads from Contigs”
+
 magicblast -db $d -sra $s -num_threads 2 | samtools view -bS - | samtools sort -o $s.bam
 
 samtools view $s.bam | awk '{OFS="\t"; print ">"$1"\n"$10}' > $s.fasta
-rm $s.bam
 
 echo “Assembling Reads”
+
 megahit -r $s.fasta -o $s
 
 rm $s.fasta
@@ -47,40 +45,50 @@ mv $s/final.contigs.fa zero_$s.fa
 rm -r $s
 
 ## For Loop Here (i = 1-5)
-
 echo "Trim Ends and Make Database"
 
 ./contig2blastdb.pl -f zero_$s.fa -d ends
 
 echo "Second Magic BLAST"
+
 magicblast -db ends -sra $s -num_threads 2 | samtools view -bS - | samtools sort -o $s.bam
 
-samtools view $s.bam | awk '{OFS="\t"; print ">"$1"\n"$10}' > $s.fa
+samtools view $s.bam | awk '{OFS="\t"; print ">"$1"\n"$10}' > $s.fa 
+
 rm $s.bam
 
 echo "Combine fa files"
 
+
+## Change the Combination (Remove Contigs from Other Assemblies)
 cat *.fa > $s_combined_ends.fasta
 
 echo "Second Megahit Run"
+
 megahit -r $s_combined_ends.fasta -o $s
 
 mv $s/final.contigs.fa one_$s.fa
+
 rm -r $s
 
 mkdir extended_contigs
+
 cd extended_contigs/
 
 echo "Split fasta files"
 
 awk '/^>/ {OUT=substr($0,2) ".fa"}; {print >> OUT; close(OUT)}' ../one_$s.fa
 
-rename 's/\s.*$//' *
+rename 's/\s.*$//' *.fasta
 
 echo "Perform Extend Match"
-for f in *; do ../contig_extend_match.pl -q $f -c ../zero_SRR1553459.fa -x ../extended_$f.fasta; done;
 
+for f in *
+do ../contig_extend_match.pl -q $f -c ../zero_SRR1553459.fa -x ../extended_$f.fasta
+done
+ 
 cd ..
+
 mkdir fasta_ext
 
 mv extended_*.fasta fasta_ext/
