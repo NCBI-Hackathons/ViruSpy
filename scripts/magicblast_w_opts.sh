802a23d0d6c45cb56b17c9717668348b45833b6a
Paul Cantalupo <pcantalupo@gmail.com>
2017-10-07 12:03:47 -0400

    added support for -query option to magicblast; still need to implement -infmt option for fastq files

--- scripts/magicblast_w_opts.sh
+++ scripts/magicblast_w_opts.sh
@@ -8,15 +8,19 @@ IFS=$'\n\t'
 #module load samtools/1.5-gcc5.2.0
 #module load magic-blast
 
+srr=""
+query=""
 fasta=""
 blastDB=""
-while getopts f:b:s:o:p: option
+out_file=""
+while getopts f:b:s:q:o:p: option
 do
 case "${option}"
 	in
 	f) fasta=${OPTARG};;
 	b) blastDB=${OPTARG};;
 	s) srr=${OPTARG};;
+	q) query=${OPTARG};;
 	o) out_file=${OPTARG};;
 	p) paired=$true;;
 esac
@@ -24,60 +28,77 @@ done
 
 viralDB=./viral.db
 
-if ! [[ -z $fasta ]] && ! [[ -z $blastDB ]]; then
-	echo -e "\nMagicBlast: Script can only accept one ViralDB option (fasta or blastDB).\n"; exit
+if [[ ! -z $fasta && ! -z $blastDB ]]; then
+	echo -e "\n$0: BLAST database (-b) and fasta file (-f) options are incompatible with each other.\n"; exit
+elif [[ ( ! -z $query && ! -z $srr ) || ( -z $query && -z $srr ) ]]; then
+	echo -e "\n$0: please provide either an SRR (-s) or query sequence file (-q).\n"; exit
 elif ! [[ -z $fasta ]]; then
-	echo -e "\nConverting '$fasta' to BlastDB..."
+	echo -e "\n$0: converting '$fasta' to BlastDB..."
 	makeblastdb -dbtype nucl -in $fasta -parse_seqids -out $viralDB
 elif ! [[ -z $blastDB ]]; then
-	echo -e "\nUsing '$blastDB' as BlastDB.\n"
+	echo -e "\n$0: using '$blastDB' as BlastDB.\n"
 	viralDB=$blastDB
 else
-	echo -e "\nUsing Viral RefSeq genomic sequences from NCBI as default viral database"
+	echo -e "\n$0: using Viral RefSeq genomic sequences from NCBI as default viral database"
 	virussequences=viral.all.1.genomic.fna
 	wget.refseq.sh $virussequences
 	makeblastdb -dbtype nucl -in $virussequences -parse_seqids -out viral.db
 fi
 
-if [[ -z $srr ]]; then
-	echo -e "\nMagic Blast: No SRR value provided; exiting."; exit
-else
-	echo -e "\nRunning MAGICBLAST on $srr,"
-	echo -e "saving to '$out_file'\n"
+INPUT=""
+echo -en "\n$0: running Magic-BLAST on "
+if [[ ! -z $srr ]]; then
+	echo $srr
+elif [[ ! -z $query ]]; then
+	echo $query
 fi
+echo $INPUT
 
 if [[ -z $out_file ]]; then
 	out_file="./putative_viral_reads.fastq"
-	echo -e "\nMagicBlast: No output file provided, saving reads to '$out_file.fastq.'\n"
 fi
+echo -e "$0: saving to '$out_file'\n"
 
 #srr=SRR1161435 west nile viurs
 #srr=SRR5675673 #amazon river metagenomics
 
 num_threads=2
+out=temp_out
 out_format=sam
 word_size=20
 percent_id_cuttoff=60
 
 ##This is for paired end reads
 # added '-no_unaligned' since new version of MB 1.3 outputs all reads even if unaligned
-magicblast 	-db $viralDB \
-		-sra $srr \
-		-paired \
-		-no_unaligned \
-		-num_threads $num_threads \
-		-out temp_out.sam \
-		-outfmt $out_format \
-		-word_size $word_size \
-		-perc_identity $percent_id_cuttoff
+if [[ ! -z $srr ]]; then
+	magicblast 	-db $viralDB \
+			-sra $srr \
+			-paired \
+			-no_unaligned \
+			-num_threads $num_threads \
+			-out $out.sam \
+			-outfmt $out_format \
+			-word_size $word_size \
+			-perc_identity $percent_id_cuttoff
+else
+	magicblast 	-db $viralDB \
+			-query $query \
+			-paired \
+			-no_unaligned \
+			-num_threads $num_threads \
+			-out $out.sam \
+			-outfmt $out_format \
+			-word_size $word_size \
+			-perc_identity $percent_id_cuttoff
+fi
 
 #Samtools
 #covert back to fasta format
 
-echo -e "Converting putative viral reads back to FASTQ format...\n"
+echo -n "Converting putative viral reads back to FASTQ format..."
 
-samtools view -bS temp_out.sam > temp_out.bam
+samtools view -bS $out.sam > $out.bam
 # -v 40 is for character 'I' in the quality string
-samtools fastq temp_out.bam -v 40 > $out_file
+samtools fastq $out.bam -v 40 > $out_file
 
-#rm ./temp_out.*
+rm ./$out.*
